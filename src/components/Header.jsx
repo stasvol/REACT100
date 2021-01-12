06acf51b0dfe55271cf1027cb0886e7195b68274
stasvol <git@github.com:stasvol/ToDo.git>
2021-01-13 01:08:26 +0200

    create app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import classes from './Header.module.css';
+
+const Header = () =>{
+    return(
+
+            <header className={classes.header}>
+                <img src={'https://img2.pngio.com/logo-png-images-download-150000-logo-png-resources-with-logo-download-png-360_360.png'}/>
+            </header>
+    )
+}
+export default Header
