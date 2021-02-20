96b4aa2f84286d132f8672bc619c01062ea3eb80
stasvol <git@github.com:stasvol/ToDo.git>
2021-02-20 20:34:37 +0200

    commit update file

--- src/components/Profile/ProfilInfo/ProfilInfo.js
+++ src/components/Profile/ProfilInfo/ProfilInfo.js
@@ -6,48 +6,102 @@ import ProfilStatus from "./ProfilStatus";
 import ProfilStatusWithHook from "./ProfilStatusWithHook";
 import kot from "../../../Photo/Images/kot.png"
 import ProfilStatusHook from "./ProfilStatusWithHook";
+import {useState} from "react";
 
 
 const ProfInfo = (props) => {
 
-       if (!props.profile){
-           return <Preloader />
-       }
- const onPhotoChange = (e)  => {
-      if (e.target.files.length) {
-          props.savePhoto(e.target.files[0])
-      }
- }
+    const [editMode, setEditMode] = useState(false);
+
+    if (!props.profile) {
+        return <Preloader/>
+    }
+    const onPhotoChange = (e) => {
+        if (e.target.files.length) {
+            props.savePhoto(e.target.files[0])
+        }
+    }
     return (
-        <div>
+        <div className={classes.contact}>
             {/*<div>*/}
             {/*    <img className={classes.imgCont} alt={'image'}*/}
             {/*         src={'https://sites.google.com/site/prirodanasevseegooglgfgf/_/rsrc/1463456237313/home/priroda_gory_nebo_ozero_oblaka_81150_1920x1080.jpg'}/>*/}
             {/*</div>*/}
-            <div >
+            <div>
                 <ProfilStatusHook status={props.status} updateStatus={props.updateStatus}/>
-                    {/*<ProfilStatus status={props.status} updateStatus={props.updateStatus}/>*/}
+                {/*<ProfilStatus status={props.status} updateStatus={props.updateStatus}/>*/}
             </div>
             <div>
-                  <img src={props.profile.photos.small || kot } className={classes.image} alt={'image'} />
+                <img src={props.profile.photos.small || kot} className={classes.image} alt={'image'}/>
 
-                {props.isOwner  &&  <input type={"file"} onChange={onPhotoChange}/> }
+                {props.isOwner && <input type={"file"} onChange={onPhotoChange}/>}
 
             </div>
-            <div>
-                <div>About Me : {props.profile.aboutMe}</div>
-                  <h4>Contacts : </h4>
-                <div> Facebook : {props.profile.contacts.facebook}</div>
-                <div> VK : {props.profile.contacts.vk}</div>
-                <div> Twitter : {props.profile.contacts.twitter}</div>
-                <div> Instagram : {props.profile.contacts.instagram}</div>
-                <div> Git Hub : {props.profile.contacts.github}</div>
-                <div> LookingForAJob : {props.profile.lookingForAJob} <img className={classes.smail}  src={smail} alt={'image'}/></div>
-                <div> LookingForAJobDescription : {props.profile.lookingForAJobDescription}</div>
-                <div> FullName : {props.profile.fullName}</div>
-            </div>
+
+            {editMode ? <ProfDataForm {...props}/> : <ProfData {...props} /> }
+
+            {/*<div>*/}
+
+            {/*    <div> <b>FullName</b> : {props.profile.fullName}</div>*/}
+            {/*    <div> <b>About Me</b> : {props.profile.aboutMe}</div>*/}
+            {/*    <div> <b>LookingForAJob</b> : {props.profile.lookingForAJob ? 'Yes' : 'No'}*/}
+            {/*        <img className={classes.smail}  src={smail} alt={'image'}/>*/}
+            {/*    </div>*/}
+            {/*    {props.profile.lookingForAJob &&*/}
+            {/*    <div> <b>My professional skills</b> : {props.profile.lookingForAJobDescription}</div>*/}
+            {/*    }*/}
+            {/*    <h4><b>Contacts</b> : </h4> {Object.keys(props.profile.contacts).map(key =>{*/}
+            {/*          return <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]} />*/}
+            {/*})}*/}
+
+            {/*    /!*<div> Facebook : {props.profile.contacts.facebook}</div>*!/*/}
+            {/*    /!*<div> VK : {props.profile.contacts.vk}</div>*!/*/}
+            {/*    /!*<div> Twitter : {props.profile.contacts.twitter}</div>*!/*/}
+            {/*    /!*<div> Instagram : {props.profile.contacts.instagram}</div>*!/*/}
+            {/*    /!*<div> Git Hub : {props.profile.contacts.github}</div>*!/*/}
+            {/*    /!*<div> Youtube : {props.profile.contacts.Youtube}</div>*!/*/}
+            {/*    /!*<div>MainLink : {props.profile.mainLink}</div>*!/*/}
+            {/*    /!*<div> FullName : {props.profile.fullName}</div>*!/*/}
+            {/*    /!*<div>Photos : {props.profile.photos} </div>*!/*/}
+            {/*</div>*/}
         </div>
     )
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+    return <div><b>{contactTitle}</b> : {contactValue} </div>
+}
+
+const ProfData = ({ ...props}) => {
+
+    return <div>
+
+                    <div><b>FullName</b> : {props.profile.fullName}</div>
+                    <div><b>About Me</b> : {props.profile.aboutMe}</div>
+                    <div><b>LookingForAJob</b> : {props.profile.lookingForAJob ? 'Yes' : 'No'}
+                        <img className={classes.smail} src={smail} alt={'image'}/>
+                    </div>
+                 {props.profile.lookingForAJob &&
+                 <div><b>My professional skills</b> : {props.profile.lookingForAJobDescription}</div>
+                 }
+                 <h4><b>Contacts</b> : </h4> {Object.keys(props.profile.contacts).map(key => {
+                 return <Contact key={key} contactTitle={key} contactValue={props.profile.contacts[key]}/>
+                })}
+
+                          {/*<div> Facebook : {props.profile.contacts.facebook}</div>*/}
+                          {/*<div> VK : {props.profile.contacts.vk}</div>*/}
+                          {/*<div> Twitter : {props.profile.contacts.twitter}</div>*/}
+                          {/*<div> Instagram : {props.profile.contacts.instagram}</div>*/}
+                          {/*<div> Git Hub : {props.profile.contacts.github}</div>*/}
+                          {/*<div> Youtube : {props.profile.contacts.Youtube}</div>*/}
+                          {/*<div>MainLink : {props.profile.mainLink}</div>*/}
+                          {/*<div> FullName : {props.profile.fullName}</div>*/}
+                          {/*<div>Photos : {props.profile.photos} </div>*/}
+    </div>
+
+}
+const ProfDataForm = ({ ...props}) => {
+
 }
 
 export default ProfInfo
